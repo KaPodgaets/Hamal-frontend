f517d48848c43b1003b6471f22efd98a2814b21b
Kosta P <kapodgaets@gmail.com>
2025-06-18 13:00:04 +0300

    static port for app

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3030, // 👈 change to your desired port
+    strictPort: true, // 👈 fail if the port is taken instead of falling back
+    host: true, // 👈 optional: allows access from local network or Docker
+  },
+});
