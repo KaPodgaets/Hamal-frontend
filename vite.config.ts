09e9f89de7eec69dc65cca1446d3c3243f9c80f1
Podgaets Konstantin <kapodgaets@gmail.com>
2025-06-21 11:50:00 +0300

    fix port for better dev env testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3030, // 👈 change to your desired port
+    port: 5174, // 👈 change to your desired port
     strictPort: true, // 👈 fail if the port is taken instead of falling back
     host: true, // 👈 optional: allows access from local network or Docker
   },
